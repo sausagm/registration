d93f12b4d92ecad7e153df98f5529842ba6c285a
sausagm <mesilov2011@mail.ru>
2019-03-27 10:04:22 +0300

    Initial commit

---
+++ microblog/app/routes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Медвед'}
+    posts = [
+        {
+            'author': {'username': 'Олег'},
+            'body': 'Здарова, бандиты!'
+        },
+        {
+            'author':{'username': 'Саня'},
+            'body': 'Я обЬелся!'
+        },
+        {
+            'author':{'username':'Диоген'},
+            'body':'Живу в коробке, и не парит!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('Login.html', title='Sign In', form=form)
