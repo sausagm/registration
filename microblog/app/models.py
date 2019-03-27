5b9090bd8991ae5debef8c786209abb834c9872f
sausagm <mesilov2011@mail.ru>
2019-03-27 11:02:18 +0300

    1

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,6 +1,9 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
-class User(db.Model):
+from flask_login import UserMixin
+from app import login
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -8,6 +11,11 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     def __repr__(self):
         return '<User {}>' .format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -15,3 +23,6 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     def __repr_(self):
         return '<Post {}>' .format(self.body)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
