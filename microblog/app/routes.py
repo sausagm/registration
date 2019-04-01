dbaea4216c2b5c2ad33536229eb959c6077d36a1
sausagm <mesilov2011@mail.ru>
2019-04-01 14:36:32 +0300

    1

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,15 +2,15 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
-from flask_login import current_user, login_user
+from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User
-from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Медвед'}
     posts = [
         {
             'author': {'username': 'Олег'},
@@ -25,7 +25,9 @@ def index():
             'body':'Живу в коробке, и не парит!'
         }
     ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    return render_template("index.html", title='Home', posts=posts)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -42,10 +44,14 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -56,6 +62,15 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registere user!')
+        flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_temmplate('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form)
+@app.route ('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
