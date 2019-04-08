45012aa8fa43e0d44136cff188244a9d2d6ca065
sausagm <mesilov2011@mail.ru>
2019-04-08 12:36:41 +0300

    2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -17,12 +25,12 @@ def index():
             'body': 'Здарова, бандиты!'
         },
         {
-            'author':{'username': 'Саня'},
+            'author': {'username': 'Саня'},
             'body': 'Я обЬелся!'
         },
         {
-            'author':{'username':'Диоген'},
-            'body':'Живу в коробке, и не парит!'
+            'author': {'username': 'Диоген'},
+            'body': 'Живу в коробке, и не парит!'
         }
     ]
     return render_template("index.html", title='Home', posts=posts)
@@ -34,7 +42,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(usernamne=form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
@@ -65,12 +73,30 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-@app.route ('/user/<username>')
+
+
+@app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
+        {'author': user, 'body': 'Test post#1'},
+        {'author': user, 'body': 'Test post#2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
